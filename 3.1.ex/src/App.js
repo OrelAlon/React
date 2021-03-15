940aaa4737f872f0502bf2e225694fff43bfaf36
orel alon <orela231089@gmail.com>
2021-03-15 18:06:45 +0200

    add ex 3

---
+++ 3.1.ex/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Box1 from './components/Box1';
+
+function App() {
+  return (
+    <div className='container'>
+      <Box1></Box1>
+    </div>
+  );
+}
+export default App;
